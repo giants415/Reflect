d84bf4855d8d803f5a02a8c7f8a9b19f33c5d141
Andrew <andrew.vinocur@gmail.com>
2016-10-26 13:36:01 -0700

    adds initial resources routes and basic user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def show
+    @user = User.find_by_id(user_id)
+    @posts = Post.where(user_id: user_id)
+  end
+
+  def edit
+    @user = User.find_by_id(user_id)
+  end
+
+  def update
+    user = User.find_by_id(user_id)
+    user.update_attributes(user_params)
+    redirect_to user_path(user)
+  end
+
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
+
+  def user_id
+    params[:id]
+  end
 end
