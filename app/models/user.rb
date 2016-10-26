e07d2f640e4436321a4aa8e18d638746e924bfb9
Andrew <andrew.vinocur@gmail.com>
2016-10-26 15:45:00 -0700

    adds self confirm to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
 
-
+  def self.confirm(params)
+    @user = User.find_by({username: params[:username]})
+    @user.try(:authenticate, params[:password])
+  end
 end
