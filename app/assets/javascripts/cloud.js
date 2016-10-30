1d6acc5400f33db0b65ef4940b0b33fa0060ea6d
Andrew <andrew.vinocur@gmail.com>
2016-10-29 18:48:15 -0700

    working to pass post word freq data to cloud controller: attempting to add reference

--- app/assets/javascripts/cloud.js
+++ app/assets/javascripts/cloud.js
@@ -2,19 +2,8 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-var word_array = [
-    {text: "Lorem", weight: 15},
-    {text: "Ipsum", weight: 9},
-    {text: "Dolor", weight: 6},
-    {text: "Sit", weight: 7},
-    {text: "Amet", weight: 5},
-    {text: "Moose", weight: 15},
-    {text: "Potato", weight: 9},
-    {text: "Almond", weight: 6},
-    {text: "Avocado", weight: 7},
-    {text: "Pants", weight: 5}
-    // ...as many words as you want
-];
+console.log(post.find_by_id(48).words_freq);
+// debugger
 
   $(function() {
     // When DOM is ready, select the container element and call the jQCloud method, passing the array of words as the first argument.
--- app/controllers/cloud_controller.rb
+++ app/controllers/cloud_controller.rb
@@ -1,7 +1,13 @@
 class CloudController < ApplicationController
 
   def index
+    @posts = Post.where(post_id: post_id)
+  end
 
+  private
+
+  def post_id
+    params[:id]
   end
 
 end
