9eb87fe420737be288b72cd19b893f17d00e7616
Andrew <andrew.vinocur@gmail.com>
2016-10-29 17:17:45 -0700

    word cloud will only target div w/ id target

--- app/assets/javascripts/cloud.js
+++ app/assets/javascripts/cloud.js
@@ -7,14 +7,16 @@ var word_array = [
     {text: "Ipsum", weight: 9},
     {text: "Dolor", weight: 6},
     {text: "Sit", weight: 7},
-    {text: "Amet", weight: 5}
+    {text: "Amet", weight: 5},
+    {text: "Moose", weight: 15},
+    {text: "Potato", weight: 9},
+    {text: "Almond", weight: 6},
+    {text: "Avocado", weight: 7},
+    {text: "Pants", weight: 5}
     // ...as many words as you want
 ];
 
   $(function() {
-
-
     // When DOM is ready, select the container element and call the jQCloud method, passing the array of words as the first argument.
-    console.log("jqcloud call to build");
-    $("div").jQCloud(word_array, {width: 500, height: 500});
+    $("#target").jQCloud(word_array, {width: 500, height: 500});
   });
--- vendor/assets/javascripts/jqcloud.js
+++ vendor/assets/javascripts/jqcloud.js
@@ -9,7 +9,6 @@
  * Date: 2013-05-09 18:54:22 +0200
 */
 
-console.log("jqcloud lib");
 
 (function( $ ) {
   "use strict";
