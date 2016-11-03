0a66fabeaebe2c2112c6c1a6a4f6be5d54d05adf
Andrew <andrew.vinocur@gmail.com>
2016-11-03 11:56:27 -0700

    removes unused, commented out code

--- app/assets/javascripts/cloud.js
+++ app/assets/javascripts/cloud.js
@@ -1,11 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-
-// console.log(post.find_by_id(48).words_freq);
-// debugger
-
-  // $(function() {
-  //   // When DOM is ready, select the container element and call the jQCloud method, passing the array of words as the first argument.
-  //   $("#target").jQCloud(word_array, {width: 500, height: 500});
-  // });
